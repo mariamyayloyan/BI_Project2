eb9c93324ae93cd1cdedd10e2b3f2316e821c4dd
mariamyayloyan <mariamyay@gmail.com>
2024-12-14 23:36:13 +0400

    python changes

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-#from pipeline_relational_data.flow import RelationalDataFlow
 from pipeline_dimensional_data.flow import DimensionalDataFlow
 
-
 if __name__ == "__main__":
-    # relational_data_flow = RelationalDataFlow()
-    
-    # try:
-    #     relational_data_flow.exec()
-    # finally:
-    #     relational_data_flow.close_connection()
-    
     dimensional_data_flow = DimensionalDataFlow()
     
     try:
-        dimensional_data_flow.exec()
+        #start_date = "2024-01-01"
+        #end_date = "2024-12-31"
+        start_date = None
+        end_date = None
+        dimensional_data_flow.exec(start_date=start_date, end_date=end_date)
     finally:
         dimensional_data_flow.close_connection()
