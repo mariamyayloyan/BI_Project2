9ea3a5888c246cd42422685042bb4a784c6790af
HaSahakyan <92768238+HaSahakyan@users.noreply.github.com>
2024-12-14 13:54:20 +0400

    Complete config.py file

--- pipeline_dimensional_data/config.py
+++ pipeline_dimensional_data/config.py
@@ -1,1 +1,10 @@
 # Pipeline Configuration
+def set_input_folder(task_type):
+    if task_type == "create_table":
+        input_dir = 'C:\\Users\\hasmik\\Downloads\\BI-GP2\\infrastructure_initiation'
+    elif task_type == "update":
+        input_dir = 'C:\\Users\\hasmik\\Downloads\\BI-GP2\\pipeline_dimensional_data\\queries'
+    return input_dir
+
+sql_server_config = 'C:\\Users\\hasmik\\Downloads\\BI-GP2\\sql_server_config.cfg'
+print('##### Choekout config.py #####')
